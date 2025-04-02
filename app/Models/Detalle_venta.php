b384848d337d215e50ec920106f8eb84f2d29355
Rafael <rkovrkovrkov@gmail.com>
2025-04-02 10:01:13 -0400

    modified:   app/Models/Cita.php 	modified:   app/Models/Detalle_venta.php

--- app/Models/Detalle_venta.php
+++ app/Models/Detalle_venta.php
@@ -10,64 +10,74 @@ class Detalle_venta extends Model
     protected $fillable = [
         'venta_id',
         'tipo_item',
+        'item_id',
         'cantidad',
-        'fecha_movimiento',
-        'descripcion',
+        'precio_unitario',
+        'subtotal',
     ];
     public function producto()
     {
-        return $this->belongsTo(Producto::class, 'producto_id', 'producto_id');
+        return $this->belongsTo(Producto::class, 'item_id', 'producto_id');
     }
 //============================================================================
     public static function get_labels(){
         return [
             'data' => [
                 [   'display-name' => 'ID',
-                    'name' => 'movimiento_id',
+                    'name' => 'detalle_id',
                     'type' => 'auto',],
                 [   'display-name' => 'Tipo',
-                    'name' => 'tipo_movimiento',
+                    'name' => 'tipo_item',
                     'type' => 'enum',
                     'enum' => [
-                        ['display-name' => 'Entrada', 'name' => 'entrada'],
-                        ['display-name' => 'Salida', 'name' => 'salida'],
+                        ['display-name' => 'Servicio', 'name' => 'servicio'],
+                        ['display-name' => 'Producto', 'name' => 'producto'],
                 ],],
                 [   'display-name' => 'Cantidad',
                     'name' => 'cantidad',
                     'type' => 'number',],
-                [   'display-name' => 'Fecha',
-                    'name' => 'fecha_movimiento',
-                    'type' => 'date',],
-                [   'display-name' => 'Descripcion',
-                    'name' => 'descripcion',
-                    'type' => 'textarea',],
+                [   'display-name' => 'Precio unitario',
+                    'name' => 'precio_unitario',
+                    'type' => 'decimal',],
+                [   'display-name' => 'Sub total',
+                    'name' => 'subtotal',
+                    'type' => 'decimal',],
             ],
         ];
     }
     public static function get_validate(){
         return [
-            'tipo_movimiento' => ['required','string', 'max:64'],
+            'tipo_item' => ['required','string', 'max:64'],
             'cantidad' => ['required', 'integer','min:0','max:999999'],
-            'fecha_movimiento' => ['required', 'string', 'max:10'],
-            'descripcion' => ['required','string', 'max:255'],
-            'producto_id' => ['required', 'exists:productos,producto_id'],
+            'precio_unitario' => ['required', 'numeric','min:0','max:999999'],
+            'subtotal' => ['required', 'numeric','min:0','max:999999'],
+            'item_id' => ['required', 'exists:productos,producto_id'],
+            'venta_id' => ['required', 'exists:ventas,venta_id'],
         ];
     }
     public static function get_fkLabels(){
         return [
             [
                 'name' => 'Producto',
-                'attr' => 'producto_id',
+                'attr' => 'item_id',
                 'fk_name' => 'nombre',
                 'fk_id' => 'producto_id',
                 'data' => Producto::select('producto_id', 'nombre')->get(),
+            ],
+            [
+                'name' => 'Venta',
+                'attr' => 'venta_id',
+                'fk_name' => 'venta_id',
+                'fk_id' => 'venta_id',
+                'data' => Venta::select('venta_id')->get(),
             ],
         ];
     }
     public function get_fk()
     {
         return [
-            'producto_id' => $this->producto->nombre,
+            'item_id' => $this->producto->nombre,
+            'venta_id' => $this->venta_id,
         ];
     }
     //============================================================================
