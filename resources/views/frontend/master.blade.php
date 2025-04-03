21689b3213a9f7ec88d9baa8df28d1bd67ef4b56
Juan Ramos <rangulegamer@gmail.com>
2025-04-03 08:58:11 -0400

    direcciones

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -55,11 +55,11 @@
                                 <div class="main-menu f-right d-none d-lg-block">
                                     <nav>
                                         <ul id="navigation">
-                                            <li class="active"><a href="index.html">Home</a></li>
-                                            <li><a href="{{ url('/about') }}">About</a></li>
-                                            <li><a href="{{ url('/services') }}">Services</a></li>
-                                            <li><a href="{{ url('/portfolio')}}">Portfolio</a></li>
-                                            <li><a href="{{ url('/blog') }}">Blog</a>
+                                            <li class="active"><a href="{{ url('index.html')}}">Home</a></li>
+                                            <li><a href="{{ url('/about.html') }}">About</a></li>
+                                            <li><a href="{{ url('/services.html') }}">Services</a></li>
+                                            <li><a href="{{ url('/portfolio.html')}}">Portfolio</a></li>
+                                            <li><a href="{{ url('/blog.html') }}">Blog</a>
                                                 <ul class="submenu">
                                                     <li><a href="blog.html">Blog</a></li>
                                                     <li><a href="blog_details.html">Blog Details</a></li>
